1b22bbc4887a47f1e36555971ed88df2fda473cb
Devjagrat <devjagrat@gmail.com>
2026-04-23 00:01:46 +0530

    Add more changes to Addition file

--- Addition.cpp
+++ Addition.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int x=10,y=90;
-    cout<<x+y<<endl;
+    int n, sum=0,i;
+    cout<<"how many numbers you want to add ?"<<endl;
+    cin>>n;
+    cout<<"Enter the number"<<endl;
+    while(n){
+        cin>>i;
+        sum += i;
+        n--;
+        if(n>0){
+            cout<<"Next number ?"<<endl;
+        }
+        else{
+            cout<<"Number count up"<<endl;
+        }
+    }
+    cout<<"Your sum is: "<<sum<<endl;
     return 0;
 }
